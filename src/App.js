7e1dd99ee58a3110fc0e2108e870820038d95157
Eduardo Tornquist <eatornquist04@gmail.com>
2023-08-28 14:30:15 -0400

    Hiding some page sections without specific use so far.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import About from './components/About'
 import Facts from './components/Facts'
 import Skills from './components/Skills'
 import Resume from './components/Resume'
-import Portfolio from './components/Portfolio'
-import Services from './components/Services'
-import Testimonials from './components/Testimonials'
+// import Portfolio from './components/Portfolio'
+// import Services from './components/Services'
+// import Testimonials from './components/Testimonials'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import Header from './components/Header'
@@ -20,20 +20,20 @@ function App() {
         <Facts />
         <Skills />
         <Resume />
-        <Portfolio />
-        <Services />
-        <Testimonials />
+        {/* <Portfolio /> */}
+        {/* <Services /> */}
+        {/* <Testimonials /> */}
         <Contact />
       </main>
 
       <Footer />
 
-      <a
+      {/* <a
         href="#"
         className="back-to-top d-flex align-items-center justify-content-center"
       >
         <i className="bi bi-arrow-up-short"></i>
-      </a>
+      </a> */}
     </>
   )
 }
