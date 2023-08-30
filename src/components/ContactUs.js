67f5c62a5a0f9bd5b479a7f84833bc533fc202bd
Eduardo Tornquist <eatornquist04@gmail.com>
2023-08-30 15:54:27 -0400

    fixing a console warning.

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 import emailjs from '@emailjs/browser'
-import { send } from 'emailjs-com'
+// import { send } from 'emailjs-com'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
@@ -75,7 +75,6 @@ export const ContactUs = () => {
 
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
-                  // frameBorder="0"
                   style={{ border: '0', with: '100%', height: '290px' }}
                   allowFullScreen
                 ></iframe>
